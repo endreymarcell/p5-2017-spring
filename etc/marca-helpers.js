14a3858328a0f27f6f8e1d7d1191fa539a3cb637
Marcell Endrey <marcell.endrey@prezi.com>
2017-03-04 10:30:51 +0100

    let's version control helpers as well

---
+++ etc/marca-helpers.js
@@ -0,0 +1,23 @@
+(function(){
+  var corsProxy = 'https://cors-anywhere.herokuapp.com/'; // 'https://crossorigin.me/' seems to be down
+
+  window.playSound = function(sound) {
+    if (!sound.isPlaying()) {
+      sound.play();
+    }
+  }
+
+  window.stopSound = function(sound) {
+    sound.stop();
+  }
+
+  window.loadsound = function(url) {
+      return loadSound(corsProxy + url);
+  }
+
+  window.loadimage = function(url) {
+      return loadImage(corsProxy + url);
+  }
+
+  window.print = console.log;
+})();
