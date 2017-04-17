ad84b8f8e3b7a0e7168347d0e6eff2a524d68d19
Marcell Endrey <marcell.endrey@prezi.com>
2017-04-17 18:54:42 +0200

    simplify helpers file

--- etc/marca-helpers.js
+++ etc/marca-helpers.js
@@ -1,67 +1,65 @@
-(function(){
-  var corsProxy = 'https://cors-anywhere.herokuapp.com/'; // 'https://crossorigin.me/' seems to be down
+corsProxy = "https://cors-anywhere.herokuapp.com/" // "https://crossorigin.me/" seems to be down
 
-  window.playSound = function(sound) {
-    if (!sound.isPlaying()) {
-      sound.play();
-    }
+function playSound(sound) {
+  if (!sound.isPlaying()) {
+    sound.play()
   }
+}
 
-  window.stopSound = function(sound) {
-    sound.stop();
+function stopSound(sound) {
+  sound.stop()
+}
+
+function loadsound(url) {
+  return loadSound(corsProxy + url)
+}
+
+function loadimage(url) {
+  return loadImage(corsProxy + url)
+}
+
+print = console.log
+
+function circle(x, y, d) {
+  ellipse(x, y, d, d)
+}
+
+function square(x, y, a) {
+  rect(x, y, a, a)
+}
+
+function pulse(from, to, speed) {
+  if (typeof speed === "undefined") {
+    speed = 1
   }
+  var middle = (from + to) / 2
+  var amplitude = Math.abs(from - to) / 2
+  var direction = to > from ? 1 : -1
+  return middle + direction * amplitude * sin(frameCount / 50 * speed)
+}
 
-  window.loadsound = function(url) {
-    return loadSound(corsProxy + url);
+function oneway(from, to, speed) {
+  if (typeof speed === "undefined") {
+    speed = 1
   }
+  var distance = Math.abs(from - to)
+  var direction = to > from ? 1 : -1
+  return from + direction * (frameCount * speed) % distance
+}
 
-  window.loadimage = function(url) {
-    return loadImage(corsProxy + url);
+function twoway(from, to, speed) {
+  if (typeof speed === "undefined") {
+    speed = 1
   }
-
-  window.print = console.log;
-
-  window.circle = function(x, y, d) {
-    ellipse(x, y, d, d);
+  var distance = Math.abs(from - to)
+  var full = (frameCount * speed) % (distance * 2)
+  if (full > distance) {
+    full = 2 * distance - full
   }
+  var direction = to > from ? 1 : -1
+  return from + direction * full
+}
 
-  window.square = function(x, y, a) {
-    rect(x, y, a, a);
-  }
-
-  window.pulse = function(from, to, speed) {
-    if (typeof speed === 'undefined') {
-      speed = 1
-    }
-    var middle = (from + to) / 2
-    var amplitude = Math.abs(from - to) / 2
-    var direction = to > from ? 1 : -1
-    return middle + direction * amplitude * sin(frameCount / 50 * speed)
-  }
-
-  window.oneway = function(from, to, speed) {
-    if (typeof speed === 'undefined') {
-      speed = 1
-    }
-    var distance = Math.abs(from - to)
-    var direction = to > from ? 1 : -1
-    return from + direction * (frameCount * speed) % distance
-  }
-
-  window.twoway = function(from, to, speed) {
-    if (typeof speed === 'undefined') {
-      speed = 1
-    }
-    var distance = Math.abs(from - to)
-    var full = (frameCount * speed) % (distance * 2)
-    if (full > distance) {
-      full = 2 * distance - full
-    }
-    var direction = to > from ? 1 : -1
-    return from + direction * full
-  }
-
-  window.createGroup = function() {
-    return new Group();
-  }
-})();
+function createGroup() {
+  return new Group()
+}
